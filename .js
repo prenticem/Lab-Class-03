a4c02150f8c149515c6da0d83c24630ae5236909
Peanut <123972373+prenticem@users.noreply.github.com>
2023-04-06 01:36:41 -0500

    update

---
+++ .js
@@ -0,0 +1,15 @@
+var gopher = 1;
+if (gopher < 10)
+    console.log(gopher);{
+} else {
+    console.log('potato');
+}
+
+
+cookieCount *= 5;
+
+1 =! loneliestNumber;
+
+userName = userName.toUpperCase();
+
+var NumBooks;
